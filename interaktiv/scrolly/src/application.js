43457bae5b2088c1e7addfce23a921552d4b56ec
fin <fin+github@xbhd.org>
2018-05-23 18:44:52 +0200

    update

---
+++ interaktiv/scrolly/src/application.js
@@ -0,0 +1,270 @@
+'use strict';
+require('./leaflet.fusesearch.09e7508.css?modules=false');
+require('./style.css?modules=false');
+require('leaflet-responsive-popup/leaflet.responsive.popup.css?modules=false')
+
+
+import {getEmbed} from './pymembed';
+window.getEmbed = getEmbed;
+
+import L from 'leaflet';
+import LS from 'leaflet-sleep';
+import URI from 'urijs';
+import * as request from 'd3-request/index';
+import * as format from 'd3-format/index';
+import * as queue from 'd3-queue/index';
+import * as scale from 'd3-scale/index';
+import * as topojson  from 'topojson/index';
+import './leaflet.fusesearch.09e7508';
+import './leaflet.pattern.d543c9f';
+import 'leaflet-responsive-popup';
+
+
+
+import {maps} from './data';
+
+import {legend} from './legend';
+
+var last_json = null;
+var last_data = null;
+var last_layer = null;
+var last_legend = null;
+var layer_style = null;
+
+
+
+var pymChild = new pym.Child();
+
+
+format.formatDefaultLocale({decimal: ",", thousands: ".", grouping: [3], currency: ['€ ','']})
+var numfmt = format.format(',d');
+var pctfmt = format.format(',.2r');
+
+var map = L.map('graph',
+  {
+    wakeMessage: 'Karte mit Klick aktivieren',
+    wakeMessageTouch: 'Karte mit Berührung aktivieren',
+    sleepOpacity: .95,
+    hoverToWake: false,
+    zoomSnap: 0.25
+  });
+map.zoomControl.setPosition('bottomright');
+var searchCtrl = L.control.fuseSearch({maxResultLength: 6, placeholder: 'Gemeindesuche', title: 'Gemeindesuche'});
+
+
+map.createPane('popup2',map._container);
+map.on('move', function() {
+  map._panes['popup2'].style.transform = map._mapPane.style.transform;
+});
+
+
+
+
+
+
+function change_map(MAP) {
+  var colorlegend = legend[MAP.scale](MAP);
+  if(MAP.baselayer) {
+    MAP.baselayer().addTo(map).bringToBack();
+  }
+  Array.prototype.map.bind(document.querySelectorAll('h1')
+    )((e) => e.innerHTML = PARAMS.bundesland && MAP.bundesland_title ? MAP.bundesland_title : MAP.title);
+  Array.prototype.map.bind(document.querySelectorAll('footer .actual_source')
+    )((e) => e.innerHTML = MAP.source);
+  Array.prototype.map.bind(document.querySelectorAll('p.detail')
+    )((e) => e.innerHTML = MAP.detail || '');
+  Array.prototype.map.bind(document.querySelectorAll('body p.description')
+    )((e) => e.innerHTML = MAP.description || '');
+
+  if(PARAMS.link) {
+    document.getElementsByTagName('h1')[0].innerHTML=`<a href="${PARAMS.link}" target="_blank">${document.getElementsByTagName('h1')[0].innerHTML}</a>`;
+  }
+
+
+  if(PARAMS.force_message || (PARAMS.bundesland && MAP.bundesland_message)) {
+    document.querySelector('#bundesland_message').innerHTML = MAP.bundesland_message;
+  }
+
+
+  queue.queue()
+    .defer(request.json, MAP.topojson)
+    .defer(request[MAP.data.split('.').reverse()[0]], MAP.data)
+    .await(function(error, topo, data){
+      var os = topo.objects[Object.keys(topo.objects)[0]];
+
+      if(colorlegend['set_data']) {
+        colorlegend.set_data(data);
+      }
+
+      layer_style = function(feature) {
+              var r = {
+                  color: 'white',
+                  weight: L.Browser.retina?0.25:0.5,
+                  opacity: 0.75,
+                  fillOpacity: 0.75
+                };
+              var fill = feature.data?colorlegend.getColor(feature.data):{fillColor: 'lightgrey'};
+              for(var k of Object.keys(fill)) {
+                if(k=='fillPattern') {
+                  fill[k].addTo(map);
+                }
+                r[k] = fill[k];
+              }
+              return r;
+              };
+      if(PARAMS.bundesland || MAP.bundesland) {
+        os.geometries = os.geometries.filter((x) => x.properties.GKZ[0]==(PARAMS.bundesland||MAP.bundesland));
+      }
+      var tf = topojson.feature(topo, os);
+      if(!last_layer || last_data!=MAP.data || last_json!=MAP.topojson) {
+        var layer;
+        layer = L.geoJson(
+          tf, {
+            smoothFactor: L.Browser.retina?0.5:1,
+            onEachFeature: function(feature,thislayer) {
+              feature.layer = thislayer;
+              thislayer.on({
+                'mouseover': (e) => {e.target.setStyle({weight: 1.5})},
+                'mouseout': (e) => {e.target.setStyle(layer_style(feature))}
+              });
+            }
+          });
+
+        layer.addTo(map);
+        last_layer = layer;
+        last_data = MAP.data;
+        last_json = MAP.topojson;
+      }
+      for(let [k,thislayer] of Object.entries(last_layer._layers)) {
+        var feature = thislayer.feature;
+        feature.data = data.filter((x) => x[MAP.data_key]==feature.properties[MAP.property_key]);
+
+        var p = feature.properties;
+        var d = feature.data;
+        if(!d) {
+          return;
+        }
+        thislayer.bindPopup(L.responsivePopup().setContent(
+          MAP.tooltip(d,p,pctfmt,numfmt)
+        ),{pane: 'popup2'});
+      }
+      last_layer.setStyle(layer_style);
+      if(MAP.is_gemeinden) {
+        var bm_key = (x) => PARAMS.bundesland?x.properties[MAP.property_key].slice(0,3):x.properties[MAP.property_key][0];
+        var bm = topojson.mesh(topo,
+          topo.objects[Object.keys(topo.objects)[0]],
+            (a,b) => bm_key(a)!==bm_key(b)
+        );
+        var blayer = L.geoJson(
+          [bm],
+          {style: {fillColor: 'transparent',
+            fillOpacity: 0, color: 'white', weight: 2, opacity: 1,
+          attribution: 'Grenzen: cc-by Geoland.at, Wien.gv.at'}}
+        );
+        blayer.addTo(map);
+      }
+
+      if(MAP.search) {
+        searchCtrl.indexFeatures(tf,MAP.search_keys);
+        searchCtrl.addTo(map);
+        document.getElementById('controls').appendChild(searchCtrl._container);
+        searchCtrl._container.children[0].innerHTML=MAP.search_title;
+        document.querySelector('a.button').style.visibility=MAP.search===false?'hidden':'visible';
+      }
+
+
+      if(last_legend) {
+        last_legend.remove();
+      }
+      colorlegend.addTo(map);
+      last_legend = colorlegend;
+
+      var b = last_layer.getBounds()
+      map.fitBounds(b, {
+        paddingTopLeft: [0,60],
+        paddingBottomRight: [0,25],
+        animate: false
+      });
+      map.setMaxBounds(map.getBounds().pad(5));
+      map.options.minZoom = map.getZoom()-0.5;
+      map.fire('zoomend');
+
+      pymChild.sendHeight();
+  });
+
+}
+
+
+var PARAMS = URI.parseQuery(document.location.search)
+var MAP = maps[PARAMS.map];
+if(MAP) {
+  change_map(MAP);
+
+  /*
+  setTimeout(() => {
+    setInterval(() => change_map(maps['kindergaerten']), 10000);
+  },5000)
+  setInterval(() => change_map(maps['kindergaerten2']), 10000);
+  */
+}
+
+(() => {
+  var popup_highlight = null;
+  var popup_line = null;
+
+  map.on('popupopen', function(e) {
+    if(popup_highlight) {
+      map.removeLayer(popup_highlight);
+      popup_highlight=null;
+    }
+    if(popup_line) {
+      map.removeLayer(popup_line);
+      popup_line=null;
+    }
+    if(map._container.clientWidth<500) {
+      document.getElementById('info').innerHTML = e.popup._content;
+      document.getElementById('info').style.maxHeight = 999+'px';
+      map.closePopup();
+      var t = map.containerPointToLatLng([map._size.x/2, 0]);
+      popup_line = L.polyline([[e.popup._latlng.lat,e.popup._latlng.lng],
+        t], {color: '#f1f1f1',weight: 2, opacity: 0.7}).addTo(map);
+      if(MAP.search) {
+        document.querySelector('a.button').scrollIntoView();
+      }
+      pymChild.scrollParentToChildPos(
+        document.getElementById('info').getBoundingClientRect().top + window.pageYOffset - 125
+      );
+    } else {
+      document.getElementById('info').innerHTML = '';
+      document.getElementById('info').style.maxHeight = "0";
+    }
+    popup_highlight = L.geoJson(
+      e.popup._source.feature.layer.toGeoJSON(),
+      {style: {weight: 1.75, color: 'white', fillColor: 'transparent'}}).addTo(map);
+    pymChild.sendHeight();
+  });
+  map.on('popupclose', function(e) {
+    if(map._container.clientWidth<500){
+      return;
+    }
+    if(popup_highlight) {
+      map.removeLayer(popup_highlight);
+    }
+    if(popup_line) {
+      map.removeLayer(popup_line);
+    }
+  });
+  map.on('move', (e) => {
+    if(popup_line) {
+      map.removeLayer(popup_line);
+      popup_line=null;
+    }
+  });
+})();
+
+
+window.addEventListener('resize', function() {
+  pymChild.sendHeight();
+})
+
+module.exports = {change_map: change_map, maps: maps};
---
+++ interaktiv/scrolly/src/data.js
@@ -0,0 +1,134 @@
+var maps = {};
+import * as d3scale from 'd3-scale';
+var cs = ['#381f41', '#664273','#a79cab','#41624b'];
+var cs2 = ['yellow', 'orange','red','green'];
+var ts = d3scale.scaleThreshold().domain([-25,-0.1,0.1]).range(['großer Rückgang','Rückgang','keine Veränderung','Zuwachs']);
+
+maps['kindergaerten'] = {
+  title: 'Wo Eltern ihre Kinder abgemeldet haben',
+  description: 'In dieser Karte wird die prozentuelle Veränderung der Anzahl der Kindergartenkinder in Nachmittagsbetreuung zwischen Jänner und März/April 2018 dargestellt. Gemeinden, die keine Auskunft gegeben oder keine Nachmittagsbetreuung haben werden gestreift dargestellt.',
+  detail: 'In einigen Gemeinden gibt es <span class="scalevalue">keine Betreuung/Kooperation</span> oder nur eine Betreuung durch <span class="scalevalue">Caritas oder andere private Erhalter</span>, manche haben <span class="scalevalue">keine Auskunft</span> gegeben.',
+  data: 'gemeinden.csv',
+  data_key: 'gkz',
+  property_key: 'GKZ',
+  source: 'Eigenrecherche',
+  is_gemeinden: false,
+  search_keys: ['PG'],
+  search_title: 'Gemeindesuche',
+  topojson: 'gemeinden.topojson',
+  bundesland: 4,
+  value: function(d) {
+    if(d.length==0) {
+      return null;
+    }
+    d = d[0];
+    if(d.Kategorie=='Alles da') {
+      return [0,ts(d['Prozentuelle Änderung'])];
+    }
+    return [1,d.Kategorie];
+  },
+
+  scale: 'threshold-or-category',
+
+  search: true,
+  colorschemes: [
+    cs,
+    ['#888888','#000000', '#820B0B']
+  ],
+  legend: 0,
+  thresholds: {colors: cs, thresholds: ts.domain(), labels: ts.range()},
+
+  categories: [['großer Rückgang', 'Rückgang', 'keine Veränderung', 'Zuwachs'],
+    ['keine Betreuung/Kooperation', 'Caritas oder andere private Erhalter', 'keine Auskunft']],
+  /*baselayer: () => L.tileLayer('https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg', {
+      subdomains: ["maps", "maps1", "maps2", "maps3"],
+      attribution: 'cc-by 3.0 basemap.at',
+      detectRetina: true
+  }) ,*/
+
+  tooltip: function(d, p, pctfmt, numfmt) {
+    if(d.length==0) {
+      return p.PG;
+    }
+    d = d[0];
+    return `<strong>${d.name}</strong>`+
+      (d.Kategorie=='Alles da'?`
+      <span style="padding-left: 0.5em; font-weight: bold; font-size: 1.25em; float: right; display: inline-block;">${d['Prozentuelle Änderung']>0?'+':''}${pctfmt(d['Prozentuelle Änderung'])} %</span>
+<br />
+Kindergartenkinder: ${numfmt(d['Kinder insgesamt'])}<br />
+Kinder am Nachmittag vor Gebühr: ${numfmt(d['Jänner: Kinder in Nachmittagsbetreuung'])}<br />
+Kinder am Nachmittag nach Gebühr: ${numfmt(d['März: Kinder in Nachmittagsbetreuung'])}<br />
+Veränderung: ${d.Differenz>0?'+':''}${numfmt(d['Differenz'])}
+        `:(
+          d.Kategorie=='keine Auskunft'?`<br />
+          Mitarbeiter der Gemeinde ${d.name} haben das Beantworten unserer Fragen mehrfach abgelehnt.
+          Womöglich haben Sie mehr Glück: Rufen die Gemeinde unter ${d.Telefonnummer} an und teilen Sie uns die
+          Rückmeldung über <a target="_blank" href="https://form.jotformeu.com/81265006658357">dieses Formular</a> mit.
+`:`<br />${d.Kategorie}`))+ `
+    ${d['Karte-Text']?'<br /><span class="kartetext">'+d['Karte-Text']+'</span>':''}`;
+  }
+};
+
+
+maps['kinderbetreuung'] = {
+  title: 'Kindergarten-Öffnungszeiten in Österreich',
+  description: 'Öffnungszeiten von Einrichtungen, die Kinder von 3 – 6-Jährigen betreuen. Gibt es mehrere Einrichtungen, wird die durchschnittliche Öffnungszeit, gewichtet nach der Anzahl der Kinder, angezeigt.',
+  detail: '',
+  data: 'gemeinden_kinderbetreuung.csv',
+  data_key: 'gkz',
+  property_key: 'GKZ',
+  source: 'Statistik Austria',
+  is_gemeinden: true,
+  search_keys: ['name'],
+  search_title: 'Gemeindesuche',
+  topojson: 'gemeinden m bezirke 2018.topojson',
+
+  scale: 'zero-and-linear',
+
+  numfmt: (d) => {
+    var hours = Math.floor(d/60);
+    var minutes = Math.round(d-hours*60);
+    return hours+':'+(minutes<10?'0':'')+minutes+' Stunden';
+  },
+
+  value: function(d) {
+    if(d.length==0) {
+      return 0;
+    }
+    return parseFloat(d[0]['gemgew16']);
+  },
+
+
+  search: true,
+  colorschemes: ['#f2f2f2'].concat(['#DCD7DE','#40234b']),
+  legend: 0,
+
+  tooltip: function(d, p, pctfmt, numfmt) {
+    if(d.length==0) {
+      return p.name;
+    }
+    d = d[0];
+    var tt = `<strong>${d.name}</strong><br />`;
+    if(d.Einrsumcountstd16==0){
+      tt+='Keine Betreuungseinrichtung für 3- bis 6-jährige in der Gemeinde.';
+    } else {
+      var stunden = Math.floor(d.gemgew16/60);
+      var minuten = Math.round(d.gemgew16-stunden*60);
+      tt += `Öffnungszeit / Tag (Ø): ${stunden}:${minuten<10?'0':''}${minuten}<br />
+        Vergleich zu 2006
+        ${d.Einrsumcountstd16 >1 && d.Einrsumcountstd06>1?'*':''}
+        : ${(d.diffmin>0?"+":'')+numfmt(d.diffmin)} Minuten<br />
+        Einrichtungen: ${numfmt(d.Einrsumcountstd16)}<br />
+        Betreute Kinder: ${numfmt(d.summegewkind16)}
+      `
+      if(d.Einrsumcountstd16>1 && d.Einrsumcountstd06>1) {
+        tt += '<br />* gewichteter Wert, berücksichtigt Zahl der Kinder in den einzelnen Einrichtungen';
+      }
+    }
+    return tt;
+  }
+};
+
+Object.keys(maps).map((x) => {maps[x].map=x});
+
+export { maps };
